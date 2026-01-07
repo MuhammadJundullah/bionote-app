338419c4eacb55d6e31b683ac531249b8223964a
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2026-01-07 14:54:50 +0700

    fix: register error be

--- bionote_app/lib/config/api.dart
+++ bionote_app/lib/config/api.dart
@@ -1,7 +1,8 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://bionote-app.vercel.app',
+    // defaultValue: 'https://bionote-app.vercel.app',
+    defaultValue: 'http://localhost:3000',
   );
 
   static const String registerPath = '/auth/register';
